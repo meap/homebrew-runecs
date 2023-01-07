b84f5895efdf7484573b02e36e7ddc4ef8ef3502
meap <petr@tapmates.com>
2023-01-07 17:55:30 +0100

    Brew formula update for runecs version v0.4

--- runecs.rb
+++ runecs.rb
@@ -5,20 +5,20 @@
 class Runecs < Formula
   desc ""
   homepage "https://github.com/meap/runecs"
-  version "0.3"
+  version "0.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/meap/runecs/releases/download/v0.3/runecs_Darwin_arm64.tar.gz"
-      sha256 "f38730fd8fcbc2c7c27f1ea54490d46bda3f110b56f58e78217bd8a0d502bd10"
+      url "https://github.com/meap/runecs/releases/download/v0.4/runecs_Darwin_arm64.tar.gz"
+      sha256 "d8bbc7bda34a1b7264d5a3d85f0c4dc1f02810069ffdf673ffab383854748d94"
 
       def install
         bin.install "runecs"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/meap/runecs/releases/download/v0.3/runecs_Darwin_x86_64.tar.gz"
-      sha256 "3778069b4a3f3138839176b73b68990114d3c0e16bd2cf2888a3530de933de86"
+      url "https://github.com/meap/runecs/releases/download/v0.4/runecs_Darwin_x86_64.tar.gz"
+      sha256 "31e5e13a5b01c346a309d267727915f30cfbea699400def8e96662ffae6fbb1f"
 
       def install
         bin.install "runecs"
@@ -27,17 +27,17 @@ class Runecs < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/meap/runecs/releases/download/v0.3/runecs_Linux_arm64.tar.gz"
-      sha256 "76c4d359a78160443b4fd1f4ce65eb5a10d8e0d20d887b72ff6c78b26814a9a8"
+    if Hardware::CPU.intel?
+      url "https://github.com/meap/runecs/releases/download/v0.4/runecs_Linux_x86_64.tar.gz"
+      sha256 "4cee901d019549bdbf18aaa507337dda7a23b791c0912d1ebcc6e4b99c9b2da1"
 
       def install
         bin.install "runecs"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/meap/runecs/releases/download/v0.3/runecs_Linux_x86_64.tar.gz"
-      sha256 "9186518a6bb7e569c4bfe31a45d32c86a73ad2ae099a2c6797a09e1166f9cd74"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/meap/runecs/releases/download/v0.4/runecs_Linux_arm64.tar.gz"
+      sha256 "084219764c50735af3d5319934045c570029a3cb23bf4d637d8ad83e7d248a97"
 
       def install
         bin.install "runecs"
